ba433134ec4483a26c3b1431342c2f3444cfaef8
Rafal Klarkowski <rklarkowski@gmail.com>
2020-02-08 14:38:25 +0100

    prepare token web page & implement redirection

--- src/main/java/pl/sdacademy/userdb/UserRepositoryFactory.java
+++ src/main/java/pl/sdacademy/userdb/UserRepositoryFactory.java
@@ -1,11 +1,16 @@
 package pl.sdacademy.userdb;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class UserRepositoryFactory {
 
-    private static List<User> users = new ArrayList<>();
+    private static List<User> users = defaultUsers();
+
+    private static List<User> defaultUsers() {
+        return Arrays.asList(new User("user", "user", "George", "Martin", "+1666666666"));
+    }
 
     public static UserRepository getInstance() {
         return new UserRepository() {
