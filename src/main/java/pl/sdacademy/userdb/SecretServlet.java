0dbfb6e3f20d52809ed65515808f11f852d27527
Rafal Klarkowski <rklarkowski@gmail.com>
2020-02-08 15:17:46 +0100

    add secret web & secret servlet

---
+++ src/main/java/pl/sdacademy/userdb/SecretServlet.java
@@ -0,0 +1,28 @@
+package pl.sdacademy.userdb;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/secret")
+public class SecretServlet extends HttpServlet {
+
+    private SessionService sessionService = SessionService.getInstance();
+
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String token = req.getParameter("token");
+        User user = sessionService.getUser(token);
+
+        if (user != null) {
+            req.setAttribute("user", user);
+            req.getRequestDispatcher("secretWeb.jsp").forward(req, resp);
+        } else {
+            resp.sendRedirect("login");
+        }
+    }
+}
